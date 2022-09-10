7e221870b0d435a28d04308dab91963dd6e99eb3
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-09-10 11:39:25 +0600

    search_bar

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace
 import 'package:flutter/material.dart';
 
 import '../Widget/AppBarWidget.dart';
+import '../Widget/SearchBar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,23 +16,7 @@ class HomePage extends StatelessWidget {
           AppBarWidget(),
 
           // --------search bar-----------
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Container(
-              height: 55,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 10,
-                        offset: Offset(0, 3))
-                  ]),
-            ),
-          ),
+          SearchBar(),
         ],
       ),
     );
